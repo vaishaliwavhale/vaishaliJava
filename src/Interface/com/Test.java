4b4d8a9a0c1ca1358c0b45816910d3a7d5fbdbf8
vaishaliwavhale <138563936+vaishaliwavhale@users.noreply.github.com>
2024-05-19 17:00:08 +0530

    Update Test.java
    vaishali changes

--- src/Interface/com/Test.java
+++ src/Interface/com/Test.java
@@ -3,5 +3,7 @@ package Interface.com;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hiii");
+         System.out.println("Hello Vaishali");
+        
     }
 }
